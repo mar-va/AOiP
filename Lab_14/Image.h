a3b0a47c7ec36bffafb1cbb65c7fbddbb2db2c78
mar-va <vikamarinenkova@gmail.com>
2023-11-12 23:39:31 +0400

    commit

---
+++ Lab_14/Image.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "framework.h"
+
+void Image0(HDC hdc, int cx, int cy, COLORREF color);
+void Image1(HDC hdc, int cx, int cy, COLORREF color);
+void Vertical_Image0(HDC hdc);
+void Horizontal_Image0(HDC hdc);
+void ColStr_Image0(HDC hdc);
+void Vertical_Grad_Image0(HDC hdc);
+void Horizontal_Grad_Image0(HDC hdc);
+void ColStr_Grad_Image0(HDC hdc);
+void Image2(HDC hdc, int cx, int cy, COLORREF color);
+void Image3(HDC hdc, int cx, int cy, COLORREF color);
+void Image4(HDC hdc, int cx, int cy, COLORREF color);
+void Image5(HDC hdc, int cx, int cy, COLORREF color);
+void Image6(HDC hdc, int cx, int cy, COLORREF color);
+void Image7(HDC hdc, int cx, int cy, COLORREF color_pen, COLORREF color_brush);
